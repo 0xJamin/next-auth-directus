e26ad438318cd64292bb507050da08eec9af6b85
Jamin <okpusjamin@gmail.com>
2023-12-13 17:40:14 +0000

    refactor: fix typo error

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,7 +1,7 @@
 
 import LoginForm from "./form"
 
-export default async function RegisterPage() {
+export default async function LoginPage() {
   return (
     <div className="flex h-full justify-center flex-col min-h-screen items-center md:p-24 p-5">
       <LoginForm />
